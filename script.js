8449e6a90ace47045f901156ec2854238940c3e2
Roman Ortega Mondaca <romtega88@gmail.com>
2023-11-09 15:18:26 -0600

    nav search bar design

---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict";
+// const pokemonNumber = document.querySelector("#pokemon-number");
+// const pokemonImg = document.querySelector("#pokemon-img");
+// const pokemonName = document.querySelector("#pokemon-name");
+
+// console.log(pokemonName);
