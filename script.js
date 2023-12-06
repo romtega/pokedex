e617bf86b68a93de7d79ec7cd99d48385ee7a366
Roman Ortega Mondaca <romtega88@gmail.com>
2023-12-05 23:38:20 -0600

    DOM behavior fixed and styles improved

--- script.js
+++ script.js
@@ -2,82 +2,103 @@
 
 const maxNumPokemon = 151;
 const searchInput = document.querySelector("#search-input");
-const indexGallery = document.querySelector("#index-gallery");
-const ShowPokemon = document.querySelector("#section-card");
-const PokemonCard = document.querySelector("#card-pokemon");
-const ShowPokemonName = document.querySelector("#card-pokemon-name");
+
+const sectionIndex = document.querySelector("#section-index");
+const sectionCard = document.querySelector("#section-card");
 let allPokemons = [];
 
+function fetchPokemonData(url) {
+  return fetch(url).then((response) => response.json());
+}
+
 function displayPokemons(pokemons) {
-  indexGallery.innerHTML = "";
-  pokemons.forEach((pokemon, i, arr) => {
+  sectionIndex.innerHTML = "";
+  pokemons.forEach((pokemon, i) => {
+    const bgColor = pokemon.types[0].type.name;
     const indexGalleryHTML = `
-        <div class="index-card" data-pokemon-index="${i}">
-          <p class="pokemon-number">
-            ${i + 1} / ${arr.length}
-          </p>
-          <picture>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
-              i + 1
-            }.png" alt="${pokemon.name}">
-          </picture>
-          <h2 class="heading-secondary">${pokemon.name}</h2>
-        </div>    
+      <div class="index-card ${bgColor}" id="index-card" data-pokemon-index="${i}">
+        <p class="pokemon-number flex">#${i + 1}</p>
+        <picture>
+        <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+          i + 1
+        }.png"
+        alt="${pokemon.name}"
+        />
+        </picture>
+        <h2 class="heading-secondary">${pokemon.name}</h2>
+      </div>  
       `;
-    indexGallery.insertAdjacentHTML("beforeend", indexGalleryHTML);
+    sectionIndex.insertAdjacentHTML("beforeend", indexGalleryHTML);
   });
 }
 
-function displaySelectedPokemon(index) {
-  const pokemonName = allPokemons[index].name;
-  const pokemonNumber = parseInt(index) + 1;
-  const pokemonHeight = allPokemons[index].height;
+function openPokemonCard(e) {
+  const indexCard = e.target.closest(".index-card");
+  if (indexCard) {
+    sectionCard.classList.remove("hidden");
+    const pokemonIndex = indexCard.dataset.pokemonIndex;
+    displayPokemonCard(allPokemons[pokemonIndex]);
+  }
+}
+
+function displayPokemonCard(pokemon) {
+  const { name, id, height, weight, types } = pokemon;
+  const eachType = types.map((type) => type.type.name);
+  const bgColor = types[0].type.name;
+  console.log(pokemon);
   const cardHTML = `
-      <div class="card-pokemon" id="card-pokemon">
-        <div class="pokemon-title">
-          <h1 id="card-pokemon-name">${pokemonName}</h1>
-          <span class="pokemon-number">#${pokemonNumber}</span>
-        </div>
-        <div class="pokemon-type">
-          <span class="kind">Planta</span>
-          <span class="kind">Planta</span>
-        </div>
-        <picture>
-          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonNumber}.png" alt="${pokemonName}" />
-        </picture>
-        <div class="pokemon-about">
-          <p class="description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
-            dignissimos illo neque doloremque delectus incidunt vel, quaerat
-            accusantium non cupiditate est iusto corporis sequi nam sint
-            necessitatibus voluptatem quam quia?
-          </p>
-          <div class="pokemon-stats">
-            <p>Altura</p>
-            <span>${pokemonHeight}</span>
-            <p>Peso</p>
-            <span>20kg</span>
-          </div>
+    <div class="card-pokemon ${bgColor}">
+      <div class="pokemon-title">
+        <h2 class="heading-secondary">${name}</h2>
+        <span class="pokemon-number">${id} /<span class="pokemon-total"> ${
+    allPokemons.length
+  }</span></span>
+      </div>
+      <div class="pokemon-type">
+      ${eachType
+        .map(
+          (type) => `<span class="pokemon-type-color ${type}">${type}</span>`
+        )
+        .join("")}
+      </div>
+      <picture>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png"
+          alt=""
+        />
+      </picture>
+      <div class="pokemon-about">
+        <p class="description">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni dignissimos
+      illo neque doloremque delectus incidunt vel, quaerat accusantium non
+      cupiditate est iusto corporis sequi nam sint necessitatibus voluptatem
+      quam quia?
+        </p>
+        <div class="pokemon-stats">
+          <p>Altura</p>
+          <span>${height}</span>
+          <p>Peso</p>
+          <span>${weight}</span>
         </div>
       </div>
+    </div>
   `;
 
-  PokemonCard.innerHTML = "";
-  PokemonCard.insertAdjacentHTML("afterbegin", cardHTML);
+  sectionCard.innerHTML = "";
+  sectionCard.insertAdjacentHTML("afterbegin", cardHTML);
 }
 
 fetch(`https://pokeapi.co/api/v2/pokemon?limit=${maxNumPokemon}`)
   .then((response) => response.json())
-  .then((data) => {
-    allPokemons = data.results;
+  .then((data) =>
+    Promise.all(data.results.map((result) => fetchPokemonData(result.url)))
+  )
+  .then((pokemonData) => {
+    allPokemons = pokemonData;
+
     displayPokemons(allPokemons);
 
-    indexGallery.addEventListener("click", function (e) {
-      const card = e.target.closest(".index-card");
-      if (card) {
-        ShowPokemon.classList.remove("hidden");
-        const pokemonIndex = card.dataset.pokemonIndex;
-        displaySelectedPokemon(pokemonIndex);
-      }
-    });
-  });
+    sectionIndex.addEventListener("click", openPokemonCard);
+  })
+  .catch((error) => console.error("Error fetching Pokemon info:", error));
